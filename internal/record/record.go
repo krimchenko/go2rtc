c8c2273ee59c604e902bc6e84344351b6a89b411
krimchenko <krimchenko@mail.ru>
2025-04-24 14:14:07 +0300

    record

---
+++ internal/record/record.go
@@ -0,0 +1,76 @@
+package record
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/AlexxIT/go2rtc/internal/app"
+	"github.com/rs/zerolog"
+)
+
+var log zerolog.Logger
+var recordings = map[string]*Segments{}
+
+func Init() {
+	log = app.GetLogger("record")
+
+	var cfg struct {
+		Record  map[string]any `yaml:"record"`
+		Streams map[string]any `yaml:"streams"`
+	}
+
+	// todo defaults
+
+	app.LoadConfig(&cfg)
+
+	basePath, ok := cfg.Record["basePath"].(string)
+	if !ok {
+		log.Fatal().Msg("record.basePath is invalid")
+	}
+
+	segmentDurationStr, ok := cfg.Record["segmentDuration"].(string)
+	segmentDuration, err := time.ParseDuration(segmentDurationStr)
+	if !ok || err != nil {
+		log.Fatal().Msg("record.segmentDuration is invalid")
+	}
+
+	timezoneStr, ok := cfg.Record["timezone"].(string)
+	timezone, err := time.LoadLocation(timezoneStr)
+	if !ok || err != nil {
+		log.Fatal().Msg("record.timezone is invalid")
+	}
+
+	numSegments, ok := cfg.Record["numSegments"].(int)
+	if !ok {
+		log.Fatal().Msg("record.numSegments is invalid")
+	}
+
+	for streamName, item := range cfg.Streams {
+		switch item := item.(type) {
+		case map[string]any:
+			deviceName, ok := item["device_name"].(string)
+			if !ok {
+				continue
+			}
+			deviceName = strings.ReplaceAll(deviceName, "/", "-")
+			gateAddress := strings.Split(deviceName, " (")[0] // встретимся как попадется адрес со скобками
+			seg, err := NewSegments(
+				segmentDuration,
+				numSegments,
+				fmt.Sprintf("%s/%s/%s", basePath, gateAddress, deviceName),
+				timezone,
+				streamName,
+			)
+			if err != nil {
+				log.Fatal().Err(err).Msg("failed to create segments")
+			}
+			recordings[streamName] = seg
+
+			go seg.Record()
+			time.Sleep(time.Second * 2) // sleep couple seconds so streams won't switch segments all at the same time
+		default:
+			continue
+		}
+	}
+}
---
+++ scripts/ffmpeg_finalize.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+import sys
+
+BASE_PATH = sys.argv[1]  # /mnt/recordings
+
+
+for gate_dir in os.scandir(BASE_PATH):
+    for device_dir in os.scandir(f"{BASE_PATH}/{gate_dir.name}"):
+        recordings = [
+            r
+            for r in os.listdir(f"{BASE_PATH}/{gate_dir.name}/{device_dir.name}")
+            if r.startswith(".")
+        ]
+        recordings.sort()
+        if len(recordings) < 2:
+            continue
+
+        path = f"{BASE_PATH}/{gate_dir.name}/{device_dir.name}/{recordings[-2]}"
+        finalized_path = path.replace("_raw.mp4", "_finalized.mp4")
+        p = subprocess.Popen(
+            [
+                "ffmpeg",
+                "-y",
+                "-i",
+                path,
+                "-c",
+                "copy",
+                "-strict",
+                "-2",
+                finalized_path,
+            ],
+        )
+        return_code = p.wait()
+        if return_code > 0:
+            continue
+
+        os.remove(path)
+        os.rename(
+            finalized_path,
+            finalized_path.replace("/.", "/").replace("_finalized.mp4", ".mp4"),
+        )
+
+print("done")
